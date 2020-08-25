a74dfbaa85cde803c51a48995b30d6118058a6bd
Clay Bennington <claymation296@gmail.com>
2020-08-24 18:29:13 -0600

    This version is almost completely new under the hood though the api has not changed, except for the addition of the duration property and the tab-pages-ready event. This element has been reimagined into a virtual horizontal scroll effect instead of an actual sideways scrolling container. Slotted elements are now stacked on top of each other with absolute positioning and they are transformed in such a way as to appear to be scrolled horizontally. This resolves an issue with slotted elements that had position: sticky styles applied to them or their ancestors. This also works around an issue with Safari flashing slotted elements that have display: none set on them during vertical scrolling. Updated deps.

--- tab-pages.js
+++ tab-pages.js
@@ -12,9 +12,9 @@
   *
   **/
 
-import {AppElement, html}           from '@longlost/app-element/app-element.js';
-import {listen, schedule, unlisten} from '@longlost/utils/utils.js';
-import htmlString                   from './tab-pages.html';
+import {AppElement, html} from '@longlost/app-element/app-element.js';
+import {schedule}         from '@longlost/utils/utils.js';
+import htmlString         from './tab-pages.html';
 
 
 class TabPages extends AppElement {
@@ -33,13 +33,18 @@ class TabPages extends AppElement {
         value: 'page'
       },
 
+      duration: {
+        type: Number,
+        value: 500
+      },
+
       selected: String,
 
-      _measurements: Array,
+      _cancel: Object,
 
-      _resizeListenerKey: Object,
+      _current: String,
 
-      _start: Number
+      _nodes: Object
 
     };
   }
@@ -47,55 +52,125 @@ class TabPages extends AppElement {
 
   static get observers() {
     return [
-      '__selectedChanged(selected, _measurements, _start)'
+      '__selectedChanged(selected, _nodes)'
     ];
   }
 
 
-  connectedCallback() {
-    super.connectedCallback();
+  constructor() {
+    super();
 
-    this._resizeListenerKey = 
-      listen(window, 'resize', this.__measure.bind(this));
+    this._current = this.selected;
   }
 
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
+  async __selectedChanged(selected, nodes) {
+    if (!selected || !nodes) { return; }
 
-    unlisten(this._resizeListenerKey);
+    if (!this._current) {
+      this._current = selected;
+      return;
+    }
+
+    if (this._cancel) {
+      this._cancel();
+    }
+
+    const cancelPromise = new Promise(resolve => {
+      this._cancel = resolve;
+    });
+
+    const {index, node} = nodes[selected];
+
+    const {
+      index: previousIndex, 
+      node:  previousNode
+    } = nodes[this._current];
+
+    const direction  = index - previousIndex;
+    const x          = direction > 0 ? 100 : -100;
+    const transition = `transform ${this.duration}ms var(--custom-ease)`;
+
+    previousNode.style['transition'] = transition;
+    node.style['transform']          = `translateX(${x}%)`;
+    node.style['z-index']            = '1';
+
+    this._current = selected;
+
+    await schedule();
+
+    // NOT using `listenOnce` here since we do not want
+    // to keep a long-life reference in memory.
+    // The event won't fire if `this._cancel` is called
+    // first, thus `listenOnce` doesn't get the chance 
+    // to cleanup its cache.
+    let resolver;
+
+    const transitionPromise = new Promise(resolve => {
+      resolver = resolve;
+    });
+
+    node.addEventListener('transitionend', resolver);
+
+    previousNode.style['transform'] = `translateX(${x / -8}%)`;
+    node.style['transition']        = transition;
+    node.style['transform']         = 'unset';
+
+    // Wait for transition to finish or to be canceled before resetting.
+    await Promise.race([
+      transitionPromise,
+      cancelPromise,
+    ]);
+
+    node.removeEventListener('transitionend', resolver);
+
+    node.style['transition']         = 'unset';
+    node.style['z-index']            = '0';
+    previousNode.style['transform']  = 'unset';
+    previousNode.style['transition'] = 'unset';
+    previousNode.style['z-index']    = '-1';
+
+    this.fire('tab-pages-page-changed', {value: selected});
   }
 
 
-  __selectedChanged(selected, measurements, start) {
-    if (!selected || !measurements) { return; }
-
-    const {left} = measurements[selected];
-    const x      = left - start;
-
-    this.$.slide.style['transform'] = `translateX(${-x}px)`;
-  }
-
-
-  __done() {
-    this.fire('tab-pages-page-changed', {value: this.selected});
-  }
-
-
-  async __measure() {
+  async __setup() {
     await schedule();
 
     const nodes = this.slotNodes('#slot');
 
-    this._start = nodes[0].getBoundingClientRect().left;
+    // Set height of parent to be at least as tall as tallest slotted child node.
+    const heights = nodes.map(node => node.getBoundingClientRect().height);
+    const tallest = Math.max(...heights);
 
-    this._measurements = nodes.reduce((accum, node) => {
+    this.style['min-height'] = `${tallest}px`;
 
-      const page  = node.getAttribute(this.attrForSelected);
-      accum[page] = node.getBoundingClientRect();
+    const topIndex = this._current ? 
+      nodes.findIndex(node => 
+        node.getAttribute(this.attrForSelected) === this._current) :
+      0;
+
+    // Stack all nodes behind the one that is currently selected.
+    nodes.forEach((node, index) => {
+      if (index === topIndex) {
+        node.style['z-index'] = '0';
+      }
+      else {
+        node.style['z-index'] = '-1';
+      }
+    });
+
+    this._nodes = nodes.reduce((accum, node, index) => {
+
+      const id  = node.getAttribute(this.attrForSelected);
+      accum[id] = {index, node};
 
       return accum;       
     }, {});
+
+    await schedule();
+
+    this.fire('tab-pages-ready');
   }
 
 }
